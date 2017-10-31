c006fb5a7a2e496aedf5a15fd8e0883407d70bd0
Guillaume Ballet <gballet@gmail.com>
2017-10-30 20:30:47 -0400

    Add a transaction object

---
+++ lib/iota/transaction.ex
@@ -0,0 +1,21 @@
+defmodule Iota.Transaction do
+    @moduledoc """
+    Represents an IOTA transaction
+    """
+
+    @digits_per_address     81
+    @digits_per_bundle      81
+    @digits_per_nonce       27
+    @digits_per_signature   2187
+    @digits_per_tag         27
+
+    defstruct address:            String.duplicate("9", @digits_per_address),
+                bundle:           String.duplicate("9", @digits_per_bundle),
+                nonce:            String.duplicate("9", @digits_per_nonce),
+                signature:        String.duplicate("9", @digits_per_signature),
+                trunk:            String.duplicate("9", @digits_per_address),
+                branch:           String.duplicate("9", @digits_per_address),
+                tag:              String.duplicate("9", @digits_per_tag),
+                weight_magnitude: 0
+    @type t() :: %Iota.Transaction{address: String.t, bundle: String.t, nonce: String.t, signature: String.t, tag: String.t, branch: String.t, trunk: String.t, weight_magnitude: integer}
+end
